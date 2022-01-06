b251fcc7585a491ecf16b290667c5f9aaf6e22e0
Helio Frota <00hf11@gmail.com>
2022-01-06 16:55:07 -0300

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+function foo(a, b) {
+  return a + b;
+}
+
+function bar(x) {
+  return x + 1;
+}
+
+function fooBar(y) {
+  return y - 42;
+}
+
+module.exports = {
+  foo,
+  bar,
+  fooBar
+}
---
+++ test/foo.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fooBar = require('../index');
+
+test('Should foo', () => {
+  expect(fooBar.foo(1, 1)).toBe(2);
+});
+
+test('Should bar', () => {
+  expect(fooBar.bar(1)).toBe(2);
+});
+
+test('Should foo and bar', () => {
+  expect(fooBar.fooBar(42)).toBe(0);
+});
+
+test('aaaaa', () => {
+  expect(0).toBe(0);
+});
+
+test('bbbbb', () => {
+  expect(1).toBe(1);
+});
