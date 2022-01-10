1c25dcb6f17a3c445be75f213df48547dba1b0b2
Helio Frota <00hf11@gmail.com>
2022-01-10 15:10:38 -0300

    working with test suite

--- test/foo.test.js
+++ test/foo.test.js
@@ -2,22 +2,24 @@
 
 const fooBar = require('../index');
 
-test('Should foo', () => {
-  expect(fooBar.foo(1, 1)).toBe(2);
-});
-
-test('Should bar', () => {
-  expect(fooBar.bar(1)).toBe(2);
-});
-
-test('Should foo and bar', () => {
-  expect(fooBar.fooBar(42)).toBe(0);
-});
-
-test('aaaaa', () => {
-  expect(0).toBe(0);
-});
-
-test('bbbbb', () => {
-  expect(1).toBe(1);
+describe ('ccc', () => {
+  test('Should foo', () => {
+    expect(fooBar.foo(1, 1)).toBe(2);
+  });
+  
+  test('Should bar', () => {
+    expect(fooBar.bar(1)).toBe(2);
+  });
+  
+  test('Should foo and bar', () => {
+    expect(fooBar.fooBar(42)).toBe(0);
+  });
+  
+  test('aaaaa', () => {
+    expect(0).toBe(0);
+  });
+  
+  test('bbbbb', () => {
+    expect(1).toBe(1);
+  });
 });
